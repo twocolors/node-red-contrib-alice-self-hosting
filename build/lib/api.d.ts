516aea1fbef42e6770df860df3d1c6f63fef28c3
D <dmitry@yashin.su>
2026-08-30 13:42:39 +0300

    add: new devices

--- build/lib/api.d.ts
+++ build/lib/api.d.ts
@@ -1,4 +1,4 @@
 import { NodeServiceType } from './types';
-export declare const login: (token: string | undefined) => Promise<import("axios").AxiosResponse<any, any>>;
-export declare const callback_state: (service: NodeServiceType, device: any) => Promise<import("axios").AxiosResponse<any, any>>;
-export declare const callback_discovery: (service: NodeServiceType) => Promise<import("axios").AxiosResponse<any, any>>;
+export declare const login: (token: string | undefined) => Promise<import("axios").AxiosResponse<any, any, {}, any>>;
+export declare const callback_state: (service: NodeServiceType, device: any) => Promise<import("axios").AxiosResponse<any, any, {}, any>>;
+export declare const callback_discovery: (service: NodeServiceType) => Promise<import("axios").AxiosResponse<any, any, {}, any>>;
